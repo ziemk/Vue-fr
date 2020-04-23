899ff77a1e159dc6609eb9313dd35ff835325149
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-04-23 11:49:31 +0200

    p2c4 solution translation + delete files

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,57 +1,48 @@
 <script>
 export default {
-  name: 'MenuItem',
-  props: ['addToShoppingCart', 'image', 'inStock', 'name', 'price', 'quantity'],
-  data() {
-    return {
-      onSale: false
-    }
-  },
-  computed: {
-    generatedPrice() {
-      if (this.onSale) {
-        return (this.price * 0.9).toFixed(2)
-      } else {
-        return this.price
-      }
-    }
-  },
-  beforeMount() {
-    const today = new Date().getDate()
+	name: "MenuItem",
+	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"],
+	data() {
+		return {
+			onSale: false
+		}
+	},
+	computed: {
+		generatedPrice() {
+			if (this.onSale) {
+				return (this.price * 0.9).toFixed(2)
+			} else {
+				return this.price
+			}
+		}
+	},
+	beforeMount() {
+		const today = new Date().getDate()
 
-    if (today % 2 === 0) {
-      this.onSale = true
-    }
-  }
+		if (today % 2 === 0) {
+			this.onSale = true
+		}
+	}
 }
 </script>
 
 <template>
-<<<<<<< HEAD
-  <div class="menu-item">
-    <img class="menu-item__image" :src="image.source" :alt="image.alt" />
-=======
-  <div>
-    <h3>{{ name }}</h3>
-    <img :src="image.source" :alt="image.alt" />
-    <p>Price: {{ generatedPrice }} <span v-if="onSale">(10% off!)</span></p>
-    <p v-if="inStock">In Stock</p>
-    <p v-else>Out of Stock</p>
->>>>>>> feature: Add onSale text
-    <div>
-      <h3>{{ name }}</h3>
-      <p>Price: {{ generatedPrice }}</p>
-      <p v-if="inStock">In Stock</p>
-      <p v-else>Out of Stock</p>
-      <div>
-        <label for="add-item-quantity">Quantity: {{ quantity }}</label>
-        <input v-model.number="quantity" id="add-item-quantity" type="number" />
-        <button @click="addToShoppingCart(quantity)">
-          Add to Shopping Cart
-        </button>
-      </div>
-    </div>
-  </div>
+	<div class="menu-item">
+		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
+		<div>
+			<h3>{{ name }}</h3>
+			<p>Prix : {{ generatedPrice }}</p>
+			<p v-if="inStock">En stock</p>
+			<p v-else>En rupture de stock</p>
+			<div>
+				<label for="add-item-quantity">Quantité : {{ quantity }}</label>
+				<input v-model.number="quantity" id="add-item-quantity" type="number" />
+				<button @click="addToShoppingCart(quantity)">
+					Ajouter au panier
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style></style>
