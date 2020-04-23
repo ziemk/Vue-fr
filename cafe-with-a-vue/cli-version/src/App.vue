163c572e26e5b47fb1c757243bcc7ba43267fc71
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-04-23 11:29:02 +0200

    p2c3 begin trad change index.html

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -1,115 +1,114 @@
 <template>
-  <div id="app" class="app">
-    <h1>{{ restaurantName }}</h1>
-    <p class="description">
-      Welcome to {{ restaurantName }}! We are known for our freshly baked bread
-      and french pastries! Give you morning a warm start or treat yourself in
-      the middle of the day. Our butter is imported from local farmers in
-      France. Once you take your first bite, you will see why everyone can't get
-      enough!
-    </p>
+	<div id="app" class="app">
+		<h1>{{ restaurantName }}</h1>
+		<p class="description">
+			Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
+			notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
+			matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
+			est difficile de s'arrêter.
+		</p>
 
-    <section class="menu">
-      <h2>Menu</h2>
-      <MenuItem
-        v-for="item in simpleMenu"
-        :addToShoppingCart="addToShoppingCart"
-        :name="item.name"
-        :image="item.image"
-        :quantity="item.quantity"
-        :inStock="item.inStock"
-        :key="item.name"
-      />
-    </section>
+		<section class="menu">
+			<h2>Menu</h2>
+			<MenuItem
+				v-for="item in simpleMenu"
+				:addToShoppingCart="addToShoppingCart"
+				:name="item.name"
+				:image="item.image"
+				:quantity="item.quantity"
+				:inStock="item.inStock"
+				:key="item.name"
+			/>
+		</section>
 
-    <aside class="shopping-cart">
-      <h2>Shopping Cart: {{ shoppingCart }} items</h2>
-    </aside>
+		<aside class="shopping-cart">
+			<h2>Panier : {{ shoppingCart }} articles</h2>
+		</aside>
 
-    <h2>Contact Us</h2>
-    <p>Address: {{ address }}</p>
-    <p>Phone: {{ phone }}</p>
-    <p>Email: {{ email }}</p>
-    <p>Hours:</p>
-    <ul>
-      <li>M-F: 06:00 to 16:00</li>
-      <li>Saturday: 07:00 to 14:00</li>
-      <li>Sunday: 07:00 to 12:00</li>
-    </ul>
-    <footer class="footer">
-      <p>{{ copyright }}</p>
-    </footer>
-  </div>
+		<h2>Contactez nous</h2>
+		<p>Adresse : {{ address }}</p>
+		<p>Téléphone : {{ phone }}</p>
+		<p>Email : {{ email }}</p>
+		<p>Horaires :</p>
+		<ul>
+			<li>L-V: 06:00 à 16:00</li>
+			<li>Samedi: 07:00 à 14:00</li>
+			<li>Dimanche: 07:00 à 12:00</li>
+		</ul>
+		<footer class="footer">
+			<p>{{ copyright }}</p>
+		</footer>
+	</div>
 </template>
 
 <script>
-import MenuItem from './components/MenuItem'
+import MenuItem from "./components/MenuItem"
 
 export default {
-  name: 'App',
-  components: {
-    MenuItem
-  },
-  data() {
-    return {
-      address: '9274 Beurre Ave, Paris, France',
-      email: 'hello@cafewithavue.bakery',
-      phone: '+08 88 88 88 88 88',
-      restaurantName: 'Cafe with A Vue',
-      shoppingCart: 0,
-      simpleMenu: [
-        {
-          name: 'Crossiant',
-          image: {
-            source: '/images/crossiant.jpg',
-            alt: 'A crossiant'
-          },
-          inStock: true,
-          quantity: 1
-        },
-        {
-          name: 'French Baguette',
-          image: {
-            source: '/images/french-baguette.jpeg',
-            alt: 'Four French Baguettes'
-          },
-          inStock: true,
-          quantity: 1
-        },
-        {
-          name: 'Éclair',
-          image: {
-            source: '/images/eclair.jpg',
-            alt: 'Chocolate Éclair'
-          },
-          inStock: false,
-          quantity: 1
-        }
-      ]
-    }
-  },
-  computed: {
-    copyright() {
-      const currentYear = new Date().getFullYear()
+	name: "App",
+	components: {
+		MenuItem
+	},
+	data() {
+		return {
+			address: "18 avenue du Beurre, Paris, France",
+			email: "hello@cafewithavue.bakery",
+			phone: "01 88 88 88 88",
+			restaurantName: "La belle vue",
+			shoppingCart: 0,
+			simpleMenu: [
+				{
+					name: "Croissant",
+					image: {
+						source: "/images/crossiant.jpg",
+						alt: "Un croissant"
+					},
+					inStock: true,
+					quantity: 1
+				},
+				{
+					name: "Baguette de pain",
+					image: {
+						source: "/images/french-baguette.jpeg",
+						alt: "Quatre baguettes de pain"
+					},
+					inStock: true,
+					quantity: 1
+				},
+				{
+					name: "Éclair",
+					image: {
+						source: "/images/eclair.jpg",
+						alt: "Éclair au chocolat"
+					},
+					inStock: false,
+					quantity: 1
+				}
+			]
+		}
+	},
+	computed: {
+		copyright() {
+			const currentYear = new Date().getFullYear()
 
-      return `Copyright ${this.restaurantName} ${currentYear}`
-    }
-  },
-  methods: {
-    addToShoppingCart(amount) {
-      this.shoppingCart += amount
-    }
-  }
+			return `Copyright ${this.restaurantName} ${currentYear}`
+		}
+	},
+	methods: {
+		addToShoppingCart(amount) {
+			this.shoppingCart += amount
+		}
+	}
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
