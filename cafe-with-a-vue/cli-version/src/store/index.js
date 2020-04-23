ab53ab8987882dc7850a172e0c570cc0e6bfb9ae
Alexia Toulmet <alexia.toulmet@gmail.com>
2020-04-23 13:09:47 +0200

    p4c4 begin translation + delete files

--- cafe-with-a-vue/cli-version/src/store/index.js
+++ cafe-with-a-vue/cli-version/src/store/index.js
@@ -1,53 +1,53 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue"
+import Vuex from "vuex"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    restaurantName: 'Cafe with A Vue',
-    shoppingCart: 0,
-    simpleMenu: [
-      {
-        name: 'Crossiant',
-        image: {
-          source: '/images/crossiant.jpg',
-          alt: 'A crossiant'
-        },
-        inStock: true,
-        quantity: 1,
-        price: 2.99
-      },
-      {
-        name: 'French Baguette',
-        image: {
-          source: '/images/french-baguette.jpeg',
-          alt: 'Four French Baguettes'
-        },
-        inStock: true,
-        quantity: 1,
-        price: 3.99
-      },
-      {
-        name: 'Éclair',
-        image: {
-          source: '/images/eclair.jpg',
-          alt: 'Chocolate Éclair'
-        },
-        inStock: false,
-        quantity: 1,
-        price: 4.99
-      }
-    ]
-  },
-  getters: {
-    copyright: state => {
-      const currentYear = new Date().getFullYear()
+	state: {
+		restaurantName: "La belle vue",
+		shoppingCart: 0,
+		simpleMenu: [
+			{
+				name: "Croissant",
+				image: {
+					source: "/images/crossiant.jpg",
+					alt: "Un croissant"
+				},
+				inStock: true,
+				quantity: 1,
+				price: 2.99
+			},
+			{
+				name: "Baguette de pain",
+				image: {
+					source: "/images/french-baguette.jpeg",
+					alt: "Quatre baguettes de pain"
+				},
+				inStock: true,
+				quantity: 1,
+				price: 3.99
+			},
+			{
+				name: "Éclair",
+				image: {
+					source: "/images/eclair.jpg",
+					alt: "Éclair au chocolat"
+				},
+				inStock: false,
+				quantity: 1,
+				price: 4.99
+			}
+		]
+	},
+	getters: {
+		copyright: (state) => {
+			const currentYear = new Date().getFullYear()
 
-      return `Copyright ${state.restaurantName} ${currentYear}`
-    }
-  },
-  mutations: {},
-  actions: {},
-  modules: {}
+			return `Copyright ${state.restaurantName} ${currentYear}`
+		}
+	},
+	mutations: {},
+	actions: {},
+	modules: {}
 })
--- cafe-with-a-vue/cli-version/src/views/Home.vue
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -1,87 +1,86 @@
 <template>
-  <div>
-    <h1>{{ restaurantName }}</h1>
-    <p class="description">
-      Welcome to {{ restaurantName }}! We are known for our freshly baked bread
-      and french pastries! Give you morning a warm start or treat yourself in
-      the middle of the day. Our butter is imported from local farmers in
-      France. Once you take your first bite, you will see why everyone can't get
-      enough!
-    </p>
+	<div>
+		<h1>{{ restaurantName }}</h1>
+		<p class="description">
+			Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
+			notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
+			matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
+			est difficile de s'arrêter.
+		</p>
 
-    <section class="menu">
-      <h2>Menu</h2>
-      <MenuItem
-        v-for="item in simpleMenu"
-        @add-items-to-cart="addToShoppingCart"
-        :name="item.name"
-        :image="item.image"
-        :price="item.price"
-        :quantity="item.quantity"
-        :inStock="item.inStock"
-        :key="item.name"
-      />
-    </section>
+		<section class="menu">
+			<h2>Menu</h2>
+			<MenuItem
+				v-for="item in simpleMenu"
+				@add-items-to-cart="addToShoppingCart"
+				:name="item.name"
+				:image="item.image"
+				:price="item.price"
+				:quantity="item.quantity"
+				:inStock="item.inStock"
+				:key="item.name"
+			/>
+		</section>
 
-    <div class="shopping-cart">
-      <h2>Shopping Cart: {{ shoppingCart }} items</h2>
-    </div>
+		<div class="shopping-cart">
+			<h2>Panier: {{ shoppingCart }} articles</h2>
+		</div>
 
-    <footer class="footer">
-      <p>{{ copyright }}</p>
-    </footer>
-  </div>
+		<footer class="footer">
+			<p>{{ copyright }}</p>
+		</footer>
+	</div>
 </template>
 
 <script>
-import MenuItem from '../components/MenuItem'
-import { mapGetters, mapState } from 'vuex'
+import MenuItem from "../components/MenuItem"
+import { mapGetters, mapState } from "vuex"
 
 export default {
-  name: 'Home',
-  components: {
-    MenuItem
-  },
-  computed: {
-    ...mapGetters({
-      copyright: 'copyright'
-    }),
-    ...mapState({
-      restaurantName: 'restaurantName',
-      shoppingCart: 'shoppingCart',
-      simpleMenu: 'simpleMenu'
-    })
-  },
-  methods: {
-    addToShoppingCart(amount) {
-      this.shoppingCart += amount
-    }
-  }
+	name: "Home",
+	components: {
+		MenuItem
+	},
+	computed: {
+		...mapGetters({
+			copyright: "copyright"
+		}),
+		...mapState({
+			restaurantName: "restaurantName",
+			shoppingCart: "shoppingCart",
+			simpleMenu: "simpleMenu"
+		})
+	},
+	methods: {
+		addToShoppingCart(amount) {
+			this.shoppingCart += amount
+		}
+	}
 }
 </script>
 
 <style lang="scss">
 .description {
-  max-width: 960px;
-  font-size: 1.2rem;
-  margin: 0 auto;
+	max-width: 960px;
+	font-size: 1.2rem;
+	margin: 0 auto;
 }
 
 .footer {
-  font-style: italic;
-  text-align: center;
+	font-style: italic;
+	text-align: center;
 }
 
 .menu {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 }
 
 .shopping-cart {
-  position: absolute;
-  right: 30px;
-  top: 0;
+	position: absolute;
+	right: 30px;
+	top: 0;
 }
 </style>
